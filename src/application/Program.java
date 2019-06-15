1cbd24eebc7dfe432c15b0ff8bca0c2686680052
anderdam <anderdam@gmail.com>
2019-06-15 11:58:15 -0300

    Projeto finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,17 +3,37 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Entre o caminho completo do arquivo");
+		System.out.print("Entre o caminho completo do arquivo: ");
 		String path =sc.nextLine();
 		
 		try {BufferedReader br = new BufferedReader(new FileReader(path));
+		
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moment));
+				
+				line = br.readLine();				
+			}
+			System.out.println("Total de usuários: " + set.size());
 			
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
